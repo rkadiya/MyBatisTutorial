89fba8cbbe1be9c7ebecd334f1df786a103f1673
RAVI TEJA KADIYAM <ravitejakadiyam@RAVIs-MacBook-Pro.local>
2018-01-03 15:45:55 -0500

    First Commit

---
+++ src/myBatisInsert.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class myBatisInsert { 
+
+   public static void main(String args[]) throws IOException{
+      
+      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);		
+      SqlSession session = sqlSessionFactory.openSession();
+      
+      //Create a new student object
+      Student student = new Student(1, "Mohammad","It", 80, 984803322, "Mohammad@gmail.com" ); 
+            
+      //Insert student data      
+      session.insert("Student.insert", student);
+      System.out.println("record inserted successfully");
+      session.commit();
+      session.close();
+			
+   }
+   
+}
+
---
+++ src/mybatisDelete.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class mybatisDelete { 
+
+   public static void main(String args[]) throws IOException{
+      
+      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);		
+      SqlSession session = sqlSessionFactory.openSession(); 
+	  
+      //Delete operation
+      session.delete("Student.deleteById", 1);     
+      session.commit();
+      session.close();      
+      System.out.println("Record deleted successfully");
+			
+   }
+   
+}
